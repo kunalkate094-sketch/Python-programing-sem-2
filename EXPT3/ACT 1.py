a6df72d53e9f8e75295fc75778f0d0525bf48620
kunalkate094-sketch <kunalkate094@gmail.com>
2026-04-06 15:56:42 +0530

    Create ACT 1.py

---
+++ EXPT3/ACT 1.py
@@ -0,0 +1,10 @@
+#code for receipt
+copies = int(input("Enter number of receipt copies"))
+items = int(input("Enter number of items in each receipt:"))
+for copy in range(1, copies + 1):
+    print("\nReceipt copy:", copy)
+    print("------------")
+    for item in range(1, items + 1):
+       print("Item number:", item)
+       print("------------")
+       print("\nALL RECEIPTS PRINTED SUCESSFULLY!")
