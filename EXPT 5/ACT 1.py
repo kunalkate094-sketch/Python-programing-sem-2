224cebceb70b8e34a512b6f6a466abf5fa8e6959
kunalkate094-sketch <kunalkate094@gmail.com>
2026-04-06 16:07:47 +0530

    Create ACT 1.py

---
+++ EXPT 5/ACT 1.py
@@ -0,0 +1,15 @@
+# A shop inventory dictionary stores item name and quantity. Add new stock.
+inventory= {"Apples": 50, "Bananas": 20}
+item = "Apples"
+added_quantity = 30
+if item in inventory:
+    inventory[item] += added_quantity # Adds to existing 50
+else:
+    inventory[item] = added_quantity # Creates new entry if not found
+from collections import defaultdict
+# Initialize with int (which defaults to 0)
+inventory = defaultdict(int, {"Apples": 50,})
+# No check needed!
+inventory["Apples"] += 30
+inventory["Oranges"] += 15
+print(dict(inventory))
