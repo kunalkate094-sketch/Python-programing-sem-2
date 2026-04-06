f649de89d1e1775ada6ffbcc514936c996553912
kunalkate094-sketch <kunalkate094@gmail.com>
2026-04-06 16:04:49 +0530

    Create ACT 3.py

---
+++ EXPT 4/ACT 3.py
@@ -0,0 +1,5 @@
+# Reverse a customer feedback message.
+feedback = "Quick delivery"
+# join() merges the characters back together
+reversed_feedback = "".join(reversed(feedback))
+print(reversed_feedback)
